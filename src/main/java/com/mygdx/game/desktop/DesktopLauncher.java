143c440e5a639ab346d6c32936dbacb4f2340df9
Lawrence Joel Maureal <2401105814@student.buksu.edu.ph>
2025-10-21 22:27:15 +0800

    Fix compiler errors in DesktopLauncher.java

--- src/main/java/com/mygdx/game/desktop/DesktopLauncher.java
+++ src/main/java/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,9 +1,9 @@
 package com.mygdx.game.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
-
 public class DesktopLauncher {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
@@ -11,7 +11,6 @@ public class DesktopLauncher {
         config.setWindowedMode(800, 600);
         config.useVsync(true);
         
-<<<<<<< HEAD
         // This is safe to keep or remove, as it's not the Gdx.app call
         System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "DEBUG"); 
         
@@ -19,15 +18,12 @@ public class DesktopLauncher {
         
         // This is the CRUCIAL line. It must be the last thing before the method ends.
         new Lwjgl3Application(game, config); 
-=======
+
         // Enable debug logging
         System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "DEBUG");
-        
-        MyGdxGame game = new MyGdxGame();
         // Set libGDX debug level
         Gdx.app.setLogLevel(Gdx.app.LOG_DEBUG);
         
         new Lwjgl3Application(game, config);
->>>>>>> 25ebd588c7230c56d19f7cf0ce608c079f754c16
     }
 }
