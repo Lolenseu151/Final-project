072f86c83d848476c8d4f39e2c818962f9aec625
Lawrence Joel Maureal <2401105814@student.buksu.edu.ph>
2025-11-20 10:37:50 +0800

    Fix Scene2D import package (scenes2d -> scenes.scene2d)

--- src/main/java/com/mygdx/game/GameScreen.java
+++ src/main/java/com/mygdx/game/GameScreen.java
@@ -13,7 +13,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
