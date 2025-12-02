43f76ccb0773740a9e39ec1bc96b088d1af59753
Lawrence Joel Maureal <2401105814@student.buksu.edu.ph>
2025-12-02 16:37:31 +0800

    refactor: Clean up imports in Level5 and Level5_1 classes

--- src/main/java/com/mygdx/game/Levels/Level5_1.java
+++ src/main/java/com/mygdx/game/Levels/Level5_1.java
@@ -1,14 +1,13 @@
 package com.mygdx.game.Levels;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.Fixer;
-import com.mygdx.game.ILevelManager;
+import com.mygdx.game.ILevelManager; // fallback legacy manager
 import com.mygdx.game.LevelManager2;
-import com.mygdx.game.LevelManager; // fallback legacy manager
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * Level5_1 - edit positions to design
